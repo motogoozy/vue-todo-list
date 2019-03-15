b89d5410bec92ea719de2b67945361504c3a79a9
kspayne93 <kyle@investavante.com>
2019-03-14 23:37:00 -0600

    added router-view

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,21 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />  <!-- v-bind is passing todos data array as props to the Todos component. v-on is listening for the del-todo event (which comes from clicking the x button in TodoItem component), and when it happens the deleteTodo method is invoked. -->
+    <Header /> 
+    <router-view/>
   </div>
 </template>
 
-<script> 
-import Todos from "./components/Todos";
+<script>
+import Header from './components/layout/Header';
 
 export default {
-  name: "app",
+  name:"app",
   components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false 
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id ) //returns all todos where the id does not match the id passed as an argument (the todo that was deleted)
-    }
+    Header
   }
 }
 </script>
+
 
 <style>
   * {
@@ -51,5 +27,18 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666; 
   }
 </style>
